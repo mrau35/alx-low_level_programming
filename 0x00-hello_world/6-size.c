d4ae54b30e2883986af181af203d03f5f303863e
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-20 04:40:28 -0800

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,23 @@
  */
 int main(void)
 {
-	char charSize;
-	int intSize;
-	long long int longLongIntSize;
-	long int longIntSize;
-	float floatSize
-	
-	printf("Size of a char: %zu byte(s)\n", sizeof(charSize));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intSize));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntSize);
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntSize));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatSize));
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longLongIntType;
+	float floatType;
+
+	int charSize = sizeof(charType);
+	int intSize = sizeof(intType);
+	int longIntSize = sizeof(longIntType);
+	int longLongIntSize = sizeof(longLongIntType);
+	int floatSize = sizeof(floatType);
+
+	printf("Size of a char: %zu byte(s)\n", charSize);
+	printf("Size of an int: %zu byte(s)\n", intSize);
+	printf("Size of a long int: %zu byte(s)\n", longIntSize);
+	printf("Size of a long long int: %zu byte(s)\n", longLongIntSize);
+	printf("Size of a float: %zu byte(s)\n", floatSize);
 
 	return (0);
 }
