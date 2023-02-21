9f550d540ddd911371314ce910e6408c6d1b1208
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-21 13:42:21 -0800

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * sumof_multiples - a function that prints the sum of multiples of 3 & 5
+ * @n: number inpu
+ * Return: Always 0 (Success)
+ */
+int sumof_multiples(int n)
+{
+	int i, sum;
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	return (sum);
+}
