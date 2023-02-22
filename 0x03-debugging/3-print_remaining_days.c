01a61a51c069cd458ef025f3ce8b45c0da6dcd2a
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-22 04:55:55 -0800

    Task 3. Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/*int leapyear;*/
-
-	if ((year % 4 == 0 || year % 400 == 0))/* && !(year % 100 == 0))*/
+	if ((year % 4 == 0 || year % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -26,17 +24,6 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		/*
-		leapyear = year % 10;
-
-		if (leapyear == 0 || leapyear == 4 || leapyear == 8)
-		{
-			if (month == 2 && day == 60)
-			{
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 366 - day);
-			}
-		}*/
 		if (month == 2 && day > 59)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
