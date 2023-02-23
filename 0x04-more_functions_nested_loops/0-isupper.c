10a0d57fe080b32369224e31de85d898da3e376a
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-22 23:57:16 -0800

    Task 0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: single character input
+ * Return: 1 if c is upper 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c > = 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
