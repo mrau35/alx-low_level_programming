ca8e618f40d6446517022f93909708d1d860e0d7
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-24 03:57:18 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,30 +11,28 @@ int main(void)
 {
 	int i;
 
-	char sp[] = " ";
 	char fizz[] = "Fizz";
 	char buzz[] = "Buzz";
-	char fizzBuzz[] = "FizzBuzz"; 
+	char fizzBuzz[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("%s", fizzBuzz);
+			printf("%s ", fizzBuzz);
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("%s", fizz);
+			printf("%s ", fizz);
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("%s", buzz);
+			printf("%s ", buzz);
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
-		printf("%s", sp);
 	}
 	printf("\n");
 	return (0);
