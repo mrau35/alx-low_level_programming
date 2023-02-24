2171c125a994edbf30237fdf4d6bf22172259db0
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-24 00:35:05 -0800

    11. Largest prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -a function that prints the largest prime number
+ * @n: a number input
+ * Return: the largest prime number
+ */
+
+int main()
+{
+	long int lpn;
+
+	long int div = 2;
+	long int n = 612852475143;
+
+	while (n != 0)
+	{
+		if ((n % div) != 0)
+		{
+			div++;
+		}
+		else
+		{
+			lpn = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%ld\n", lpn);
+				break;
+			}
+		}
+	}
+	return (0);
+}
