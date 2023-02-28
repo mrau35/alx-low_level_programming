1729dfe3bab9e99c7621df86d2c353fb5917275f
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-02-28 01:37:49 -0800

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,10 @@
 
 void puts2(char *str)
 {
-	int i, len = 0;
-
-	while (*(str + len) != '\0')
+	while (*str)
 	{
-		len++;
-	}
-
-	for (i = 0; i < (len - 1); i += 2)
-	{
-		_putchar(str[i]);
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
