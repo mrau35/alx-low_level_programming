dcb4346df35d839cbb9f2d80059f788731e94c00
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-03-02 11:01:21 -0800

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,15 +15,18 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] == s2[i])
 		{
 			return (0);
+			break;
 		}
 		else if (s1[i] > s2[i])
 		{
 			return (15);
+			break;
 		}
 		else
 		{
 			return (-15);
+			break;
 		}
 	}
-	return (1);
+	return (0);
 }
