0ef5a3cbb7be5920ebc98bbe684dafee4404f086
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-03-02 11:50:55 -0800

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,18 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	if (s1[i] != '\0' && s2[i] != '\0' && i == 0)
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else
-		{
-			return (-15);
-		}
+		i++;
 	}
+	
+	if (s1[i] == s2[i])
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1[i] - s2[i]);
+	}	
 	return (0);
 }
