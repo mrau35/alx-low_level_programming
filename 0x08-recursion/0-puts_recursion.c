aaa44276ae560580899a90243c6463a0899a449c
Mohammed Rediwan <rediwanmuhe@gmail.com>
2023-03-07 23:27:33 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,18 +7,18 @@
 
 void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		return;
+	}
 	_putchar(*s);
 	s++;
-	if (*s)
+	if (*s != '\0')
 	{
 		_puts_recursion(s);
 	}
-	else if (*s == '\0')
-	{
-		_putchar('\n');
-	}
 	else
 	{
-		return;
+		_putchar('\n');
 	}
 }
